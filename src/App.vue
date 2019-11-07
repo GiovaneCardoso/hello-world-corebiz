bfd0881ce4f8a079c32d72c84b5c2141100edaf9
Giovane <giovane.cardoso@corebiz.com.br>
2019-11-07 16:09:05 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld valor=2452 msg="Hi There" textp1="Olá, esse é um paragrafo criado" textprincipal="Esse é o
+     main content da página, aqui contém os links mais importantes, 
+     além do conteudo principal do seu site, está dentro de um tag <main></main> para indicar que
+     é o seu conteúdo principal"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',        
+  components: { 
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center; 
+  color: #2c3e50;
+
+  margin-top: 60px;
+}
+</style>
